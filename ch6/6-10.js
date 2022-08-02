c1229be8ee7110869a34163e3c2d9792b626cbb6
bolonge <qhsghd5357@gmail.com>
2022-08-02 15:38:46 +0900

    6-9,10 function refactoring 6-9 관련된 함수 클래스로 묶기 6-10 관련된 함수를 변환함수로 묶

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,10 +1,26 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+import _ from "lodash";
+const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
 }
 
-export function baseRate(month, year) {
+export function enrichReading(original) {
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.texableCharge = Math.max(0, result.baseCharge - texThreshold());
+  return result;
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity;
+}
+
+function texThreshold() {
+  return 0.1;
+}
+
+function baseRate(month, year) {
   if (year === 2017 && month === 5) return 0.1;
   return 0.2;
 }
--- ch6/6-9.js
+++ ch6/6-9.js
@@ -1,10 +1,50 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+class Reading {
+  #customer;
+  #quantity;
+  #month;
+  #year;
+  construtor(data) {
+    this.#customer = data.customer;
+    this.#month = data.month;
+    this.#quantity = data.quantity;
+    this.#year = data.year;
+  }
+
+  get customer() {
+    return this.#customer;
+  }
+  get month() {
+    return this.#month;
+  }
+  get quantity() {
+    return this.#quantity;
+  }
+  get year() {
+    return this.#year;
+  }
+  get baseRate() {
+    if (this.year === 2017 && this.month === 5) return 0.1;
+    return 0.2;
+  }
+  get baseCharge() {
+    return this.baseRate * this.quantity;
+  }
+  get taxThreshold() {
+    return 0.1;
+  }
+
+  get taxableCharge() {
+    return Math.max(0, this.baseRate - this.taxThreshold);
+  }
+}
+
+const reading = new Reading({
+  customer: "ivan",
+  quantity: 10,
+  month: 5,
+  year: 2017,
+});
 
 export function acquireReading() {
   return reading;
-}
-
-export function baseRate(month, year) {
-  if (year === 2017 && month === 5) return 0.1;
-  return 0.2;
 }
