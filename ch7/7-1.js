22176dd4191b6be6b055ae78b2216e3de9987efc
bolonge <qhsghd5357@gmail.com>
2022-08-09 21:40:59 +0900

    레코드 캡슐화

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,32 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #conuntry;
+  #data;
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#conuntry = data.country;
+  }
 
-organization.name = 'Dream Coding';
+  get name() {
+    return this.#name;
+  }
+
+  get country() {
+    return this.#conuntry;
+  }
+
+  get rawData() {
+    // return { name: this.#name, country: this.#country }; //set이 있으면 이런식으로 return
+    return { ...this.#data }; // set이 없고 초기값만 return 할 수 있을때 (불변성 유지 cloneDeep())
+  }
+}
+
+const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
+
+//organization.name = "bon"; // error
 console.log(organization.name);
 console.log(organization.country);
